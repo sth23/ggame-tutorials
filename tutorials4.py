53926e15ffa07c3cadf2b8d4f81d25b4a2381a5f
sth23 <47009471+sth23@users.noreply.github.com>
2019-02-28 14:51:55 -0500

    Updated from Brython Server: 2/28/2019 2:51:58 PM

--- tutorials4.py
+++ tutorials4.py
@@ -6,6 +6,8 @@ class SpaceShip(Sprite):
     """
     asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", Frame(227,0,65,125), 4, 'vertical')
 
+    print("test")
+
     def __init__(self, position):
         super().__init__(SpaceShip.asset, position)
         self.vx = 1
@@ -48,8 +50,6 @@ class SpaceGame(App):
         noline = LineStyle(0, black)
         bg_asset = RectangleAsset(self.width, self.height, noline, black)
         bg = Sprite(bg_asset, (0,0))
-        
-        print("Test")
         
         SpaceShip((100,100))
         SpaceShip((150,150))
