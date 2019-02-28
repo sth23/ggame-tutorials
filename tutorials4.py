e6168e85f63439bf1117d644241a5fe677ba7e60
sth23 <47009471+sth23@users.noreply.github.com>
2019-02-28 14:43:56 -0500

    Updated from Brython Server: 2/28/2019 2:43:59 PM

--- tutorials4.py
+++ tutorials4.py
@@ -26,6 +26,15 @@ class SpaceShip(Sprite):
         self.x += self.vx
         self.y += self.vy
         self.rotation += self.vr
+        
+        # manage thrust animation
+        if self.thrust == 1:
+            self.setImage(self.thrustframe)
+            self.thrustframe += 1
+            if self.thrustframe == 4:
+                self.thrustframe = 1
+        else:
+            self.setImage(0)
 
 class SpaceGame(App):
     """
