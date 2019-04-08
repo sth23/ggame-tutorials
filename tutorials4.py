044897d78783aee08add49755ebd796aaedfb293
sth23 <47009471+sth23@users.noreply.github.com>
2019-04-08 14:23:04 -0400

    Updated from Brython Server: 4/8/2019 2:23:06 PM

--- tutorials4.py
+++ tutorials4.py
@@ -35,7 +35,7 @@ class SpaceShip(Sprite):
         SpaceGame.listenKeyEvent("keydown", "s", self.moveDownOn)
         SpaceGame.listenKeyEvent("keyup", "s", self.moveDownOff)
         
-        self.fxcenter = self.fycenter = 0.3
+        self.fxcenter = self.fycenter = 0.45
         
     def thrustOn(self, event):
         self.thrust = 1
